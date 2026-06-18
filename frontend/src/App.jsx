115dd8b4f5a8f1c48eccfdd1ec7164224e9afaa5
Maruf-ult <marufhossain123098@gmail.com>
2026-06-18 16:11:59 +0600

    reg & login updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
       <Routes>
         {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/reset-pass" element={<ResetPass/>}/>
 
